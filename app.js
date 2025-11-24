cbed20f8eba5f42efa011789807d298457e28a85
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-24 12:35:46 +0100

    create posts array

--- app.js
+++ app.js
@@ -2,6 +2,44 @@ const express = require ("express");
 const app = express();
 const PORT = 3000;
 
+const posts = [
+  {
+    id: 1,
+    title: "Primo post",
+    content: "Questo è il contenuto del primo post.",
+    image: "./images/ciambellone.jpeg",
+    tags: ["introduzione", "benvenuto"]
+  },
+  {
+    id: 2,
+    title: "Secondo post",
+    content: "Questo è il contenuto del secondo post.",
+    image: "./images/cracker_barbabietola.jpeg",
+    tags: ["aggiornamento", "notizie"]
+  },
+  {
+    id: 3,
+    title: "Terzo post",
+    content: "Questo è il contenuto del terzo post.",
+    image: "./iamges/pane_fritto_dolce.jpeg",
+    tags: ["evento", "annuncio"]
+  },
+  {
+    id: 4,
+    title: "Quarto post",
+    content: "Questo è il contenuto del quarto post.",
+    image: "./images/pasta_barbabietola.jpeg",
+    tags: ["tutorial", "guida"]
+  },
+  {
+    id: 5,
+    title: "Quinto post",
+    content: "Questo è il contenuto del quinto post.",
+    image: "./images/torta_paesana.jpeg",
+    tags: ["recensione", "opinione"]
+  }
+]
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port http://localhost:${PORT}`);
